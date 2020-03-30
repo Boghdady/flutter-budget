9adb9d6aa8c2693cf78e19cb2b6c9f921a03ba4c
boghdady <boghdady107@gmail.com>
2020-03-30 02:44:37 +0200

    Start Working on Category Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:fluter_budget/data/data.dart';
 import 'package:fluter_budget/helpers/color_helper.dart';
 import 'package:fluter_budget/models/category_model.dart';
+import 'package:fluter_budget/screens/category_screen.dart';
 import 'package:fluter_budget/widgets/bar_chart.dart';
 import 'package:flutter/material.dart';
 
@@ -75,81 +76,89 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildCategory(Category category, double totalAmountSpent) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      padding: EdgeInsets.all(20.0),
-      height: 100,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            offset: Offset(0, 2),
-            blurRadius: 6.0,
-          )
-        ],
+    return InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => CategoryScreen(category: category),
+        ),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                category.name,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Text(
-                '\$${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${category.maxAmount.toStringAsFixed(2)}',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 10.0),
-          // I use LayoutBuilder because i want to know container width to define other container width
-          LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              // print(constraints.maxHeight);
-              // print(constraints.maxWidth);
-              final double maxBarWidth = constraints.maxWidth;
-              final double percent =
-                  (category.maxAmount - totalAmountSpent) / category.maxAmount;
-              double barWidth = percent * maxBarWidth;
-
-              if (barWidth < 0) {
-                barWidth = 0;
-              }
-
-              return Stack(
-                children: <Widget>[
-                  Container(
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        padding: EdgeInsets.all(20.0),
+        height: 100,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            )
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  category.name,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
                   ),
-                  Container(
-                    height: 20,
-                    width: barWidth,
-                    decoration: BoxDecoration(
-                      color: getColor(context, percent),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                ),
+                Text(
+                  '\$${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${category.maxAmount.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
                   ),
-                ],
-              );
-            },
-          ),
-        ],
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            // I use LayoutBuilder because i want to know container width to define other container width
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                // print(constraints.maxHeight);
+                // print(constraints.maxWidth);
+                final double maxBarWidth = constraints.maxWidth;
+                final double percent = (category.maxAmount - totalAmountSpent) /
+                    category.maxAmount;
+                double barWidth = percent * maxBarWidth;
+
+                if (barWidth < 0) {
+                  barWidth = 0;
+                }
+
+                return Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: barWidth,
+                      decoration: BoxDecoration(
+                        color: getColor(context, percent),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
